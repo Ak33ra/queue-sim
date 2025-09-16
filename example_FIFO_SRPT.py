03a25cf46a7a6d4b3dab2db27a5680878bc4cf4e
A V <avandegr@andrew.cmu.edu>
2025-09-16 14:36:33 -0400

    clean up examples

--- example_FIFO_SRPT.py
+++ example_FIFO_SRPT.py
@@ -2,9 +2,6 @@ from queue_sim import *
 import math
 import random
 
-# def genExp(mu): # generate S ~ Exp(mu)
-    # return lambda: -(1/mu)*math.log(1-random.random())
-
 lambdas = [1.0, 1.2, 1.4, 1.6, 1.8]
 
 for l in lambdas:
--- example_MG1.py
+++ example_MG1.py
@@ -2,14 +2,12 @@ import math
 import random
 from queue_sim import *
 
+# define a custom size distribution
 def genSize():
     u = random.random()
     return 2*(5**u)
 
-def genArrival(lam):
-    return lambda:-(1/lam)*math.log(1-random.random())
-
 fcfs = FCFS(sizefn = genSize)
-system = QueueSystem([fcfs], arrivalfn = genArrival(0.5*math.log(5)/8))
+system = QueueSystem([fcfs], arrivalfn = genExp(0.5*math.log(5)/8))
 N,T = system.sim()
 print(f"N: {N}, T: {T}")
